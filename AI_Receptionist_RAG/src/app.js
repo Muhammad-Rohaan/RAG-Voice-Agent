b95ea2f47c9e0796e2ca3f4c27fdda961ac9c243
Muhammad-Rohaan <rohaanali82005@gmail.com>
2026-08-05 21:14:18 +0500

    fix: correct import casing for Render/Linux deployment

--- AI_Receptionist_RAG/src/app.js
+++ AI_Receptionist_RAG/src/app.js
@@ -3,10 +3,10 @@ import dotenv from "dotenv";
 import cors from "cors";
 import http from "http";
 import { WebSocketServer } from "ws";
-import { createChunks, loadDocs } from "../src/Controllers/RAG.controller.js";
+import { createChunks, loadDocs } from "./Controllers/RAG.controller.js";
 import { sendQueryToGroqLLM } from "./Controllers/GroqLLM.controller.js";
 // Dynamic import of startVoiceAgentSession will be done in the route handler
-import { embedding } from "./Pipes/ingestionPipeline.js";
+import { embedding } from "./Pipes/IngestionPipeline.js";
 
 
 dotenv.config();
